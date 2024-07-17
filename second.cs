d3e6fe31dceee6971cf6c5e94068464fdc5c59ad
Thapa2003 <khemthapa419@gmail.com>
2024-07-17 06:48:28 +0545

    next2

---
+++ second.cs
@@ -0,0 +1,14 @@
+using System;
+namespace second{
+class Test{
+	static void Main(string[] args){
+	Console.WriteLine("Name: Khem Bahadur Thapa");
+	Console.WriteLine("Email: khem12@gmail.com");
+	Console.WriteLine("Phone: 983676766");
+	Console.WriteLine("Address: Tikathali");
+	Console.WriteLine("Roll: 11");
+	Console.WriteLine("Program: BCA");
+	Console.ReadKey();
+	}
+	}
+}
